bf4de2920731caebf5d1cf2f97921f7aad45c437
HazemUsama <hazemosama681@gmail.com>
2023-03-17 03:11:33 +0200

    update

--- 0x00-hello_world/copy.c
+++ 0x00-hello_world/copy.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * print a phrase
- * Return: always 0
- */
+
+/* i need help */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        return (0);
 }
