945aae84525960a7c9b746c7fe008cb1eaae8ad3
HazemUsama <hazemosama681@gmail.com>
2023-03-17 02:39:50 +0200

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+/* i need help */
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
     return (0);
 }
