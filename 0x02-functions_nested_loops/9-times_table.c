7fe7731e42763c992871848f7dcb6609385573ef
HazemUsama <hazemosama681@gmail.com>
2023-03-22 04:32:48 +0200

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,6 +2,25 @@
 #include <unistd.h>
 
 /**
+* printc - function
+*
+* @c: character to compare.
+*
+* Return: void
+*
+*/
+void printc(int c)
+{
+        char ch1, ch2;
+
+        ch2 = c % 10 + '0';
+        c /= 10;
+        ch1 = c % 10 + '0';
+        _putchar(ch1);
+        _putchar(ch2);
+}
+
+/**
  * times_table - main function
  */
 void times_table(void)
