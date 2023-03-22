a388c08f971bdb72a15af339cb3075e3b397e8a3
HazemUsama <hazemosama681@gmail.com>
2023-03-22 03:46:11 +0200

    update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _isalpha - main function
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if true, 0 if false
+ *
+ */
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
