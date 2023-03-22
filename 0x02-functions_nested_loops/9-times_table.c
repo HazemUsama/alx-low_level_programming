d0391f87bd9058a62bdfe5712ca08cfe3cbe2686
HazemUsama <hazemosama681@gmail.com>
2023-03-22 04:38:01 +0200

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,30 +9,31 @@
 * Return: void
 *
 */
+
 void printc(int c)
 {
-        char ch1, ch2;
+	char ch1, ch2;
 
-        ch2 = c % 10 + '0';
-        c /= 10;
-        ch1 = c % 10 + '0';
+	ch2 = c % 10 + '0';
+	c /= 10;
+	ch1 = c % 10 + '0';
 	_putchar(',');
 	if (ch1 == '0')
-	{
 		_putchar(' ');
-	}
 	_putchar(' ');
 	if (ch1 != '0')
 		_putchar(ch1);
-        _putchar(ch2);
+	_putchar(ch2);
 }
 
 /**
  * times_table - main function
  */
+
 void times_table(void)
 {
 	int i, j;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
