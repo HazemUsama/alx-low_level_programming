486bc26937162c0411b16840a7a3d539721bc37e
HazemUsama <hazemosama681@gmail.com>
2023-03-22 04:50:31 +0200

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+	#include "main.h"
+#include <unistd.h>
+
+/**
+ * add - main function
+ *
+ * @i: character to add
+ * @j: character to add
+ * Return: the last digit of the number
+ *
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
