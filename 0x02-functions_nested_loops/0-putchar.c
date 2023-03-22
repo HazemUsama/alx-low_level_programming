07e0f3ba85a10c7183951e5da46418c4b79610d4
HazemUsama <hazemosama681@gmail.com>
2023-03-22 02:39:12 +0200

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Description: prints "_putchar"
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
