3a5e415bed563bf16a367e026597ab2134655fe1
HazemUsama <hazemosama681@gmail.com>
2023-03-22 05:01:01 +0200

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,25 +10,27 @@
  *
  */
 void print_to_98(int n) {
-    int i;
+	int i;
 
-    if (n <= 98) {
-        for (i = n; i < 98; i++) {
-            putchar(i + '0');
-            putchar(',');
-            putchar(' ');
-        }
-    } else {
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++) {
+		_putchar(i + '0');
+		_putchar(',');
+		_putchar(' ');
+		}
+	} 
+	else {
+
         for (i = n; i > 98; i--) {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(',');
-            putchar(' ');
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+		_putchar(',');
+		_putchar(' ');
         }
     }
-
-    putchar('9');
-    putchar('8');
-    putchar('\n');
+    _putchar('9');
+    _putchar('8');
+    _putchar('\n');
 }
 
