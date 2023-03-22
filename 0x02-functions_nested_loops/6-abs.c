7b9f703bd4fd752e69b1c51f09cad06586ca4630
HazemUsama <hazemosama681@gmail.com>
2023-03-22 03:58:53 +0200

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+	#include "main.h"
+#include <unistd.h>
+
+/**
+ * _abs - main function
+ *
+ * @n: character to compare.
+ *
+ * Return: the abs of the number
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return n;
+}
