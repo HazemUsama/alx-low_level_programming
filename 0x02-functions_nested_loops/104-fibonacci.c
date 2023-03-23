847c142f0d7500b979392128c8a5189aa61f7b2b
HazemUsama <hazemosama681@gmail.com>
2023-03-23 04:52:21 +0200

    update

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ */
+int  main(void)
+{
+	__int64_t i, x, y, z;
+
+	x = 0;
+	y = 1;
+
+	for (i = 0; i < 98; i++)
+	{
+		z = x + y;
+		printf("%ld", z);
+		if (i < 98)
+			printf(", ");
+		x = y;
+		y = z;
+	}
+	printf("\n");
+	return (0);
+}
