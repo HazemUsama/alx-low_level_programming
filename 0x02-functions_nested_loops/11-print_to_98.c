28eb8ac7aa3c2a6f4716d1a125d4883096a4b04b
HazemUsama <hazemosama681@gmail.com>
2023-03-23 04:11:49 +0200

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,31 +6,20 @@
  *
  * @n: character to compare.
  *
- * Return: the last digit of the number
+ * Return: print from n to 98
  *
  */
 void print_to_98(int n) {
-	int i;
 
-	if (n <= 98)
+
+	while (n != 98)
 	{
-		for (i = n; i < 98; i++) {
-		_putchar(i + '0');
-		_putchar(',');
-		_putchar(' ');
-		}
-	} 
-	else {
-
-        for (i = n; i > 98; i--) {
-		_putchar(i / 10 + '0');
-		_putchar(i % 10 + '0');
-		_putchar(',');
-		_putchar(' ');
-        }
-    }
-    _putchar('9');
-    _putchar('8');
-    _putchar('\n');
+		printf("%d, ", n);
+		if (n < 98)
+			n++;
+		else
+			n--;
+	}
+	printf("98\n");
 }
 
