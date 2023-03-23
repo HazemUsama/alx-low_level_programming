44fe2661ad838e6423a68a4db0159cf0d6a012e4
HazemUsama <hazemosama681@gmail.com>
2023-03-23 04:28:42 +0200

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,7 +34,6 @@ void printc(int c, int i)
 
 	}
 	_putchar(ch1);
-	
 }
 
 /**
@@ -46,7 +45,7 @@ void printc(int c, int i)
 void print_times_table(int n)
 {
 	int i, j;
-	
+
 	if (n < 0 || n > 15)
 		return;
 	for (i = 0; i <= n; i++)
