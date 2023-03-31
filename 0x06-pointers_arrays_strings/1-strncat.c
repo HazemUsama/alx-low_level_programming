324a03b7e85af955f3408ca9ef4ad312b6af8c89
HazemUsama <hazemosama681@gmail.com>
2023-03-31 04:36:37 +0200

    update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - appends two strings
+ *
+ * @dest: the first string
+ * @src: the second string
+ *
+ * Return: pointer to the final string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	for (int j = 0; j < n; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
