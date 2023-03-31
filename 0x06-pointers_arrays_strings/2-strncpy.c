6bb35fb148926e400e70758b8532b04ea334f880
HazemUsama <hazemosama681@gmail.com>
2023-03-31 04:42:57 +0200

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i = 0;
 
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	dest[n] = '\0';
 	return (dest);
 }
