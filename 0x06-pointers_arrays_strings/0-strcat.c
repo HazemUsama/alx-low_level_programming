192d27ed6afa456ae28a94a1e675f905835ce800
HazemUsama <hazemosama681@gmail.com>
2023-03-31 04:26:13 +0200

    update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - appends two strings
+ *
+ * @dest: the first string
+ * @src: the second string
+ *
+ * Return: pointer to the final string
+ */
+char *_strcat(char *dest, char *src);
+{
+	return (dest, src);
+}
