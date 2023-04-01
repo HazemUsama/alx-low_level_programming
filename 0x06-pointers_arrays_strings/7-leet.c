a64e5bc713d5e8e50becf354d585a34385596935
HazemUsama <hazemosama681@gmail.com>
2023-04-01 03:57:21 +0200

    update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,24 +7,19 @@
  *
  * Return: the string after conversion
  */
-char *leet(char *)
+char *leet(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	int i, j;
+	char *f = "aAeEoOtTlL";
+	char *t = "4433007711";
+	
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = 4;
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = 3;
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = 0;
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = 7;
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = 1;
-		
-		i++;
+		for (j = 0; j < 10; j++)
+		{
+			if (f[j] == s[i])
+				s[i] = t[j];
+		}
 	}
 	return (s);
 }
