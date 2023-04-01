d09b266b12797f43f350f189c624f6bc35b451b5
HazemUsama <hazemosama681@gmail.com>
2023-04-01 03:55:01 +0200

    update

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string
+ *
+ * @s: the string
+ *
+ * Return: the string after conversion
+ */
+char *leet(char *)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = 4;
+		if (s[i] == 'e' || s[i] == 'E')
+			s[i] = 3;
+		if (s[i] == 'o' || s[i] == 'O')
+			s[i] = 0;
+		if (s[i] == 't' || s[i] == 'T')
+			s[i] = 7;
+		if (s[i] == 'l' || s[i] == 'L')
+			s[i] = 1;
+		
+		i++;
+	}
+	return (s);
+}
