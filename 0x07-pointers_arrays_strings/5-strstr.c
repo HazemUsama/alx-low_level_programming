b1a386c41264cf56b90d2424f3b74819900397aa
HazemUsama <hazemosama681@gmail.com>
2023-04-11 01:08:09 +0200

    update

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strstr - searches a string for any of a set of bytes
+ *
+ * @s: the string to search into
+ * @accept: the chars we need to match
+ *
+ * Return: number of bytes in the initial segment
+ * of @s which consist only of bytes from accept
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (_strstr(haystack, needle));
+}
