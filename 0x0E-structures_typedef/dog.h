b03e64086989d770aca3c918aa17b93d323d3bd7
HazemUsama <hazemosama681@gmail.com>
2023-04-24 07:30:00 +0200

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef WOAF
+#define WOAF
+/*
+ * struct dog - dog class
+ *
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif 
+
+
