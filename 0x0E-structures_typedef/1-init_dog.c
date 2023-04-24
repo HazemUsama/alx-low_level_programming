486bbcb6fd78453731cf2cb428f213912849ba6f
HazemUsama <hazemosama681@gmail.com>
2023-04-24 19:55:52 +0200

    update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,7 +4,7 @@
 /**
  * init_dog - initialize a variable of type dog
  *
- * @d: dog struct
+ * @d: pointer to dog struct
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner's name
