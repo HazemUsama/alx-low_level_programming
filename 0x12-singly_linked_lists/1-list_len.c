e98b7268c34d88f3dcdd83955cde4f68257a13fb
HazemUsama <hazemosama681@gmail.com>
2023-04-28 03:40:16 +0200

    update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        i++;
-    }
-    return (i);
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
