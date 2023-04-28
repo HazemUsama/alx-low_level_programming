6dacecf0cbb3f17198266c099881b4952ee92fa4
HazemUsama <hazemosama681@gmail.com>
2023-04-28 22:23:30 +0200

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,6 +26,12 @@ int main(int argc, char *argv[])
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
+	    && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	result = f(a, b);
 	printf("%d\n", result);
 	return (0);
