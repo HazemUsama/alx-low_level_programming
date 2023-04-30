2bfbae3a9553106635fe2dba7b7b50c55a9e9ef9
HazemUsama <hazemosama681@gmail.com>
2023-04-30 04:23:18 +0200

    $git commit -m "sqrt recursion"

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _sqrt_helper - returns the natural square root of a number 
+ * 
+ * @low: lowest number to be evaluated
+ * @high: highest number to be evaluated
+ * @n: number to be evaluated
+ * 
+ * Return: natural square root of n or -1 if n does not have a natural square root 
+ */
+int _sqrt_helper(int low, int high, int n)
+{
+	int mid;
+
+	if (low <= high)
+	{
+		mid = (high - low) / 2 + low;
+		if (mid * mid == n)
+			return (mid);
+		if (mid * mid > n)
+			return (_sqrt_helper(low, mid - 1, n));
+		return (_sqrt_helper(mid + 1, high, n));
+	}
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to be evaluated
+ *
+ * Return: natural square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_helper(1, n, n));
+} 	
