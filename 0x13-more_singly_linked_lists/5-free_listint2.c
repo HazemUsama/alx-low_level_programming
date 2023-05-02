45e7a7fdbf284fbc2bc82444d178056b4a095a55
HazemUsama <hazemosama681@gmail.com>
2023-05-02 13:07:52 +0200

    free part two

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to the head of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	free_listint(*head);
+}
