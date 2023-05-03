b45857495f84bbb57d4bf850c2fdbb98dce9a28f
HazemUsama <hazemosama681@gmail.com>
2023-05-03 03:55:20 +0200

    idk what's wrong

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -15,13 +15,13 @@ listint_t *reverse_listint(listint_t **head)
 	prev_node = NULL;
 	next_node = NULL;
 
-	while (*(head) != NULL)
+	while (*head != NULL)
 	{
 		next_node = (*head)->next;
-		(*head) = prev_node;
-		prev_node = (*head);
-		(*head) = next_node;
+		(*head)->next = prev_node;
+		prev_node = *head;
+		*head = next_node;
 	}
-	(*head) = prev_node;
+	*head = prev_node;
 	return (*head);
 }
