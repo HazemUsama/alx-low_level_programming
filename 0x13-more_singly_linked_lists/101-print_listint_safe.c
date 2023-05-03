b96cfe5c51aa426022952733614aeab9caf6aebd
HazemUsama <hazemosama681@gmail.com>
2023-05-03 10:37:38 +0200

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,34 @@
 #include "lists.h"
+/**
+ * find_cycle - finds a cycle in a linked list 
+ * 
+ * @head: pointer to the head of the list
+ * Return: the start of the cycle, or NULL if there is no cycle
+ */
+listint_t *find_cycle(listint_t *head)
+{
+	listint_t *slow, *fast;
 
+	slow = head;
+	fast = head;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
+}
 /**
  * print_listint_safe - prints a listint_t linked list
  * @head: pointer to the head of the list
@@ -7,23 +36,29 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i;
-	const listint_t *node, *next_node;
+	size_t i, flag;
+	const listint_t *node, *end_point;
 
 	if (head == NULL)
 		exit(98);
 
 	node = head;
-	next_node = node->next;
-
-	for (i = 0; next_node != NULL && next_node < node; i++)
+	end_point = find_cycle((listint_t *)head);
+	flag = 0;
+	while (node != NULL)
 	{
 		printf("[%p] %d\n", (void *)node, node->n);
-		node = next_node;
-		next_node = next_node->next;
+		node = node->next;
+		if (node == end_point)
+		{
+			if (flag)
+			{
+				printf("-> [%p] %d\n", (void *)node, node->n);
+				return (i + 1);
+			}
+			flag = 1;
+		}
+		i++;
 	}
-	printf("[%p] %d\n", (void *)node, node->n);
-	if (next_node != NULL)
-		printf("-> [%p] %d\n", (void *)next_node, next_node->n);
-	return (i + 1);
+	return (i);
 }
