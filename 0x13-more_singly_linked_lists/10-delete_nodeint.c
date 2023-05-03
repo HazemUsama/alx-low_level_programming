7933a2383c7adb4381a4c89928e32f41686f7e3a
HazemUsama <hazemosama681@gmail.com>
2023-05-03 03:17:07 +0200

    last last fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *node, *next_node;
 	unsigned int i;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	node = *head;
