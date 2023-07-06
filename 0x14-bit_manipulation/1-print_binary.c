101ba7a885e49570732f015f45d02adf092b9977
HazemUsama <hazemosama681@gmail.com>
2023-07-06 12:03:21 +0300

    second task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: the number to pirnt
+ */
+void print_binary(unsigned long int n)
+{
+	int start = 0, i = 31;
+
+	if (!n)
+		_putchar('0');
+	while (i--)
+	{
+		if (n >> i & 1)
+			start = 1;
+		if (start)
+			_putchar('0' + (n >> i & 1));
+	}
+}
+
