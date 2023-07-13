572d497a84c48b81a3de4feb3947b37d0e49ffa3
HazemUsama <hazemosama681@gmail.com>
2023-07-13 15:13:52 +0300

    fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	file = open(filename, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
 	while (text_content && text_content[chars] != '\0')
