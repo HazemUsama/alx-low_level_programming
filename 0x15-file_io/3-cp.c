c127e2fe814a21e091684751bd9a531692565e8b
HazemUsama <hazemosama681@gmail.com>
2023-07-13 18:49:32 +0300

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+int main(int ac, char **av)
+{
+	int file_from, file_to, readLetters, printedLetters;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = open(av[1],  O_RDONLY);
+	file_to = open(av[2],  O_CREAT | O_TRUNC | O_WRONLY, 0664); 
+	
+	if (file_from == -1)
+	{
+		readError:
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",av[1]);
+		exit(98);
+	}
+	while ((readLetters = read(file_from, buffer, 1024)))
+	{
+		if (readLetters == -1)
+			goto readError;
+		printedLetters = write(file_to, buffer, readLetters);
+		if (printedLetters == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+	if (close(file_from) == -1 || close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd -1\n");
+		exit(100);
+	}
+	return(0);
+}
