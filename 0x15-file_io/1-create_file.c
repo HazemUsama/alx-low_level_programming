b2f4e2aec3bac5e652274d4ca5a25b969b51e975
HazemUsama <hazemosama681@gmail.com>
2023-07-13 15:07:01 +0300

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL || text_content == NULL)
 		return (-1);
 	file = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
 	while (text_content[chars] != '\0')
 		chars++;
 
