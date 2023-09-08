c2b7c077ba5d690964ee795790f7eecfb1dad159
HazemUsama <hazemosama681@gmail.com>
2023-09-09 02:56:39 +0300

    done

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *curr;
+	size_t i, flag = 0;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		while (curr)
+		{
+			if (flag)
+				printf(", ");
+
+			printf("\'%s\': \'%s\'", curr->key, curr->value);
+			flag = 1;
+			curr = curr->next;
+		}
+	}
+	printf("}\n");
+}
