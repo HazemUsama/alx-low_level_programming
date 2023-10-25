36f00e9e52c69a054fa04d9f49a8ed3c3b187ed0
HazemUsama <hazemosama681@gmail.com>
2023-10-25 23:28:08 +0300

    done?

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """Island Perimeter Module"""
 
+
 def island_perimeter(grid):
     """Implementation of the solution"""
-        ans = 0
-        row = [1, 0, -1, 0]
-        column = [0, 1, 0, -1]
+    ans = 0
+    row = [1, 0, -1, 0]
+    column = [0, 1, 0, -1]
 
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 0:
-                    continue
-                for k in range(4):
-                    try:
-                        r = i + row[k]
-                        c = j + column[k]
-                        if grid[r][c] == 0 or r < 0 or c < 0:
-                            ans += 1
-                    except:
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 0:
+                continue
+            for k in range(4):
+                try:
+                    r = i + row[k]
+                    c = j + column[k]
+                    if grid[r][c] == 0 or r < 0 or c < 0:
                         ans += 1
-        return ans
+                except IndexError:
+                    ans += 1
+    return ans
