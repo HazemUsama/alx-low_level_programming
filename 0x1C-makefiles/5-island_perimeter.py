c5ff975c908f1a57815461f96d6c4c4fa1897b01
HazemUsama <hazemosama681@gmail.com>
2023-10-25 23:25:38 +0300

    doc

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Island Perimeter Module"""
 
 def island_perimeter(grid):
+    """Implementation of the solution"""
         ans = 0
         row = [1, 0, -1, 0]
         column = [0, 1, 0, -1]
